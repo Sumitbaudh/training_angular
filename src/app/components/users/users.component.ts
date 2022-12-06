9f37cfdf49259dd1038254df6d86ae12d0212288
Sumitbaudh <sbsbaudh@gmail.com>
2022-12-06 07:31:09 +0530

    property binding

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,36 +11,37 @@ export class UsersComponent {
   showAddress:boolean=false;
   constructor(){
      this.users=[
-    //   {
-    //   firstName:"Harsh",
-    //   lastName:"Kumar",
-    //   age:45,
-    //   address:{
-    //      street:"5th Markup",
-    //      city:"New York",
-    //      state:"DC"
-    //   }
-    //  },
-    //  {
-    //   firstName:"Jatin",
-    //   lastName:"Sahoo",
-    //   age:35,
-    //   address:{
-    //      street:"6th New Street",
-    //      city:"New Delhi",
-    //      state:"Delhi"
-    //   }
-    //  },
-    //  {
-    //   firstName:"Manish",
-    //   lastName:"Sharma",
-    //   age:35,
-    //   address:{
-    //      street:"2nd Old Street",
-    //      city:"Lucknow",
-    //      state:"Uttar Pradesh"
-    //   }
-    //  }
+      {
+      firstName:"Harsh",
+      lastName:"Kumar",
+      age:45,
+      address:{
+         street:"5th Markup",
+         city:"New York",
+         state:"DC"
+      },
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd7l04O1EbeclTNmOqJ-2_EXWkF0PMAX3Www&usqp=CAU"
+     },
+     {
+      firstName:"Jatin",
+      lastName:"Sahoo",
+      age:35,
+      address:{
+         street:"6th New Street",
+         city:"New Delhi",
+         state:"Delhi"
+      }
+     },
+     {
+      firstName:"Manish",
+      lastName:"Sharma",
+      age:35,
+      address:{
+         street:"2nd Old Street",
+         city:"Lucknow",
+         state:"Uttar Pradesh"
+      }
+     }
     ]
    this.addressController()
   }
